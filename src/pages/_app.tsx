7b63df8c339b1a03e23bf48bda241dcc417c93c5
Цынарев Дмитрий <onehourlater1@gmail.com>
2025-04-15 12:17:28 +0300

    favicon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import Head from 'next/head'
+
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+
 import Layout from '../components/layout'
  
-// on off tailwind
-// import '@/styles/tailwind.css'
 import '@/styles/index.scss'
 
 
@@ -28,14 +29,22 @@ type AppPropsWithLayout = AppProps & {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? defaultLayout
  
-  return <>
-    <style jsx global>{`
-      html {
-        font-family: ${opensans.style.fontFamily};
-      }
-    `}
-    </style>
-   { getLayout(<Component {...pageProps} />) }
-  </>
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      <style jsx global>{`
+        html {
+          font-family: ${opensans.style.fontFamily};
+        }
+      `}
+      </style>
+      { getLayout(<Component {...pageProps} />) }
+    </>
+  )
 
 }
